7df48a5672eb7fb8eb0c1877430df600b9bef066
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-08 14:12:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,9 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      if src[row_index][element_index].even? || .odd?
-        
+      if src[row_index][element_index] % 2 == 0 
         new_string << src[row_index][element_index]
+      end
       element_index += 1 
     end
     row_index += 1 
